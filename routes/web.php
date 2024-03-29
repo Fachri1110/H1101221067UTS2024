f692be4d28d833d31bd96aa6dd5fa1282b48d010
Pahrijullll <fachri.juliandaa@gmail.com>
2024-03-29 17:22:16 +0700

    Selesai
    Done

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,36 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/masterjabatan', function () {
+    return view('masterjabatan');
+});
+
+
+Route::get('/presensi', function () {
+    return view('presensi');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/signin', function () {
+    return view('signin');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
 });
